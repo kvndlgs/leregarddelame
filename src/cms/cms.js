70739b1c340d5f4612f7f517103c8dc3638737c4
kvndlgs <es2thekay@gmail.com>
2021-03-15 20:05:24 -0400

    "3"

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -14,5 +14,5 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('service', ServicePagePreview)
+CMS.registerPreviewTemplate('services', ServicePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
