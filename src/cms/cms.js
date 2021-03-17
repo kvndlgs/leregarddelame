11b5aae611e9607925c2f384df9f086c11aa5c6f
kvndlgs <es2thekay@gmail.com>
2021-03-17 00:51:26 -0400

    yo

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,6 @@ import CMS from 'netlify-cms-app'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import ServicePagePreview from './preview-templates/ServicePagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 //CMS.registerMediaLibrary(uploadcare)
@@ -14,5 +13,4 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('services', ServicePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
